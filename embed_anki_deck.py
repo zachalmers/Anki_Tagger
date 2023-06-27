0f7173b0dab8ade07dbabf20792cca2e2c9670f0
soccerwithag <93694026+soccerwithag@users.noreply.github.com>
2023-06-27 17:26:51 -0600

    Update embed_anki_deck.py
    Adds modularity, organizes constant definitions, added progress bar to monitor embedding generation.

--- embed_anki_deck.py
+++ embed_anki_deck.py
@@ -1,59 +1,59 @@
 import os
-import openai
-import tiktoken
-import pandas as pd
-from openai.embeddings_utils import get_embedding
 
-def set_api_key():
-    try:
-        openai.api_key = os.getenv('OPENAI_API_KEY')
-    except KeyError:
-        #In terminal: export OPENAI_API_KEY=your-api-key
-        print("Set your OpenAI API key as an environment variable named 'OPENAI_API_KEY'")
+import openai
+import pandas as pd
+import tiktoken
+from openai.embeddings_utils import get_embedding
+from tqdm import tqdm
+
+# OpenAI Configuration
+OPENAI_API_KEY_ENV_VAR = 'OPENAI_API_KEY'
+EMBEDDING_MODEL = "text-embedding-ada-002"
+EMBEDDING_ENCODING = "cl100k_base"
+MAX_TOKENS = 8000
+
+def set_api_key(api_key):
+    openai.api_key = api_key
 
 def load_dataset(input_datapath):
-    if not os.path.exists(input_datapath):
-        print(f"{input_datapath} does not exist. Please check your file path.")
-        return None
+    assert os.path.exists(input_datapath), f"{input_datapath} does not exist. Please check your file path."
 
-    df = pd.read_csv(input_datapath, sep='\t', header=None, usecols=[0,1], names=["guid", "card"], comment='#')
-    df = df.dropna()
+    df = pd.read_csv(input_datapath, sep='\t', header=None, usecols=[0,1], names=["guid", "card"], comment='#').dropna()
     return df
 
-def filter_by_tokens(df, encoding, max_tokens):
+def filter_by_tokens(df, encoding):
     df["tokens"] = df.card.apply(lambda x: len(encoding.encode(x)))
-    df = df[df.tokens <= max_tokens]
-    return df
+    return df[df.tokens <= MAX_TOKENS]
 
-def calculate_embeddings(df, embedding_model):
-    df["emb"] = df.card.apply(lambda x: get_embedding(x, engine=embedding_model))
-    return df
+def calculate_embeddings(df):
+    return [get_embedding(card, engine=EMBEDDING_MODEL) for card in tqdm(df.card, desc="Calculating embeddings", dynamic_ncols=True)]
 
 def save_embeddings(df, output_prefix):
     df.to_csv(f"./{output_prefix}_embeddings.csv", index=False)
 
-if __name__ == "__main__":
-    
-    # Set OpenAI API key and embedding model parameters
-    set_api_key()
-    embedding_model = "text-embedding-ada-002"
-    embedding_encoding = "cl100k_base"  # This is the encoding for text-embedding-ada-002
-    max_tokens = 8000  # The maximum for text-embedding-ada-002 is 8191
+def main():
+    api_key = os.environ.get(OPENAI_API_KEY_ENV_VAR)
+    assert api_key, f"Set your OpenAI API key as an environment variable named '{OPENAI_API_KEY_ENV_VAR}'"
+
+    # Set OpenAI API key
+    set_api_key(api_key)
 
     # Set deck to embed.
     #This is the deck you'll apply your tags to in the end.
-    #In anki, export deck notes as plain text with guid flag checked
-    input_datapath = "./anking_notes_plaintxt_with_guid.txt"
-    output_prefix = "your_deck_name" # EDIT AS NEEDED
+    #In anki, export deck notes as plain text with GUID flag checked
+    input_datapath = "./anki.txt"
+    output_prefix = "anki" # EDIT AS NEEDED
 
     # Load and preprocess dataset
     df = load_dataset(input_datapath)
-    if df is not None:
-        encoding = tiktoken.get_encoding(embedding_encoding)
-        df = filter_by_tokens(df, encoding, max_tokens)
+    encoding = tiktoken.get_encoding(EMBEDDING_ENCODING)
+    df = filter_by_tokens(df, encoding)
 
-        # Calculate embeddings for cards
-        df = calculate_embeddings(df, embedding_model)
+    # Calculate embeddings for cards
+    df["emb"] = calculate_embeddings(df)
 
-        # Save embeddings to file
-        save_embeddings(df, output_prefix)
+    # Save embeddings to file
+    save_embeddings(df, output_prefix)
+
+if __name__ == "__main__":
+    main()
